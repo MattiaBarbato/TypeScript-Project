aee3bc2dc1be07c9b88849fc444dd140b61af7bd
Mattia Barbato <mattiabarbato@dev.com>
2022-11-15 23:38:31 +0100

    first commit

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+class Test {
+    constructor() {
+        this.lastname = 'Barbato';
+        this.name = 'Mattia';
+        this.age = 24;
+        this.power = 55;
+        this.cazzo = 44;
+        this.bestemmia = 'non carina';
+    }
+}
---
+++ index.ts
@@ -0,0 +1,16 @@
+class Test{
+    name:string;
+    lastname:string;
+    age:Number;
+    power:Number;
+    cazzo:Number;
+    bestemmia:string;
+    constructor(){
+        this.lastname = 'Barbato'
+        this.name = 'Mattia'
+        this.age = 24
+        this.power = 55
+        this.cazzo = 44
+        this.bestemmia = 'non carina'
+    }
+}
